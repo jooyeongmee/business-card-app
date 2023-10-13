462c0b4573a6fe2c27419144792e5344fda3ea87
YINGMEI <j.yeongmee@gmail.com>
2023-10-13 20:25:23 +0900

    feat: 앱 타이틀 변경

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'business car app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
